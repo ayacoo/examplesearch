8bbe167ec83a15fb7dfdc8289568038b43c38b74
Guido Schmechel <guido@ayacoo.de>
2025-02-02 13:53:37 +0100

    [TASK] Add target for ResultItemAction

--- Classes/LiveSearch/DictionaryProvider.php
+++ Classes/LiveSearch/DictionaryProvider.php
@@ -4,16 +4,16 @@ namespace Ayacoo\ExampleSearch\LiveSearch;
 
 use TYPO3\CMS\Backend\Search\LiveSearch\ResultItem;
 use TYPO3\CMS\Backend\Search\LiveSearch\ResultItemAction;
-use TYPO3\CMS\Backend\Search\LiveSearch\SearchDemand;
+use TYPO3\CMS\Backend\Search\LiveSearch\SearchDemand\SearchDemand;
 use TYPO3\CMS\Backend\Search\LiveSearch\SearchProviderInterface;
-use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Imaging\IconSize;
 
-class DictionaryProvider implements SearchProviderInterface
+readonly class DictionaryProvider implements SearchProviderInterface
 {
 
     public function __construct(
-        protected readonly IconFactory $iconFactory
+        protected IconFactory $iconFactory
     )
     {
     }
@@ -26,18 +26,20 @@ class DictionaryProvider implements SearchProviderInterface
         $resultItem = new ResultItem(self::class);
         $resultItem->setItemTitle('Google');
         $resultItem->setTypeLabel('My Dictionary');
-        $resultItem->setIcon($this->iconFactory->getIcon('mimetypes-example-search-icon', Icon::SIZE_SMALL));
+        $resultItem->setIcon($this->iconFactory->getIcon('mimetypes-example-search-icon', IconSize::SMALL));
 
         $action = new ResultItemAction('open_website');
         $action->setLabel('Open website');
-        $action->setIcon($this->iconFactory->getIcon('actions-open', Icon::SIZE_SMALL));
+        $action->setIcon($this->iconFactory->getIcon('actions-open', IconSize::SMALL));
         $action->setUrl('https://www.typo3.org');
+        $action->setTarget('_blank');
         $actions[] = $action;
 
         $action = new ResultItemAction('refresh_action');
         $action->setLabel('Refresh website');
-        $action->setIcon($this->iconFactory->getIcon('actions-refresh', Icon::SIZE_SMALL));
+        $action->setIcon($this->iconFactory->getIcon('actions-refresh', IconSize::SMALL));
         $action->setUrl('https://www.typo3.org');
+        $action->setTarget('_blank');
         $actions[] = $action;
 
         $resultItem->setActions(...$actions);
@@ -50,5 +52,10 @@ class DictionaryProvider implements SearchProviderInterface
     public function getFilterLabel(): string
     {
         return 'Dictionary records';
+    }
+
+    public function count(SearchDemand $searchDemand): int
+    {
+        return 1;
     }
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.9.99',
+            'typo3' => '14.0.0-14.9.99',
         ],
         'conflicts' => [
         ],
